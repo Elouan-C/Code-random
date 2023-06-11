8e58bcefe8c595ce00ad7882e9e03f4ef5da1494
Elouan-C <97383260+Elouan-C@users.noreply.github.com>
2023-06-11 19:32:08 +0200

    rajouter des bare de chargement

--- stl_extrusion.py
+++ stl_extrusion.py
@@ -1,12 +1,28 @@
+import os
+import threading
+import time
+import sys
+from itertools import cycle
 import numpy as np
 from stl import mesh
+from tqdm import tqdm
+
+
+def loading_spinner():
+    spinner = cycle(['-', '/', '|', '\\'])
+    while not loading_spinner.stop_event.is_set():
+        sys.stdout.write('\rChargement du STL ' + next(spinner))
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write('\r' + ' ' * 20 + '\r')
+    sys.stdout.flush()
 
 
 def find_free_edges(mesh_data):
     edges = {}
     free_edges = []
 
-    for triangle in mesh_data:
+    for triangle in tqdm(mesh_data, desc='Recherche des côtés libre'):
         for i in range(3):
             edge = frozenset([tuple(triangle[i]), tuple(triangle[(i + 1) % 3])])
             if edge in edges:
@@ -23,7 +39,7 @@ def find_free_edges(mesh_data):
 
 def create_new_edges(free_edges, fixed_z):
     new_edges = []
-    for edge in free_edges:
+    for edge in tqdm(free_edges, desc='Création de nouveau côtés'):
         v1, v2 = edge
         new_edge = [(v1[0], v1[1], fixed_z), (v2[0], v2[1], fixed_z)]
         new_edges.append(new_edge)
@@ -33,7 +49,7 @@ def create_new_edges(free_edges, fixed_z):
 def create_triangles(free_edges, new_edges):
     triangles = []
 
-    for edge in free_edges:
+    for edge in tqdm(free_edges, desc='.......Création des mures'):
         v1, v2 = edge
         new_edge = new_edges[free_edges.index(edge)]
 
@@ -53,8 +69,8 @@ def create_triangles(free_edges, new_edges):
 def create_floor(new_edges, fixed_z):
     center_point = np.mean(np.array(new_edges).reshape(-1, 3), axis=0)
     floor_triangles = []
-
-    for edge in new_edges:
+   
+    for edge in tqdm(new_edges, desc='.........Creation du sole'):
         vertex1, vertex2 = edge
         vertex3 = center_point
 
@@ -64,54 +80,73 @@ def create_floor(new_edges, fixed_z):
     return floor_triangles
 
 
-def export_stl(triangles, original_mesh, file_name):
+def export_stl(triangles, original_mesh, input_file_name):
+    base_name = os.path.basename(input_file_name)
+    output_file_name = os.path.splitext(base_name)[0] + "_extruder.stl"
+
     total_triangles = len(triangles)
-    output_mesh = mesh.Mesh(np.zeros(total_triangles, dtype=mesh.Mesh.dtype))
+    mesh_data = mesh.Mesh(np.zeros(total_triangles + len(original_mesh.vectors), dtype=mesh.Mesh.dtype))
 
     for i, triangle in enumerate(triangles):
-        output_mesh.vectors[i] = triangle
+        mesh_data.vectors[i] = triangle
 
-    output_mesh.save(file_name)
+    for i, triangle in enumerate(original_mesh.vectors):
+        offset = total_triangles
+        mesh_data.vectors[i + offset] = triangle
+
+    mesh_data.save(output_file_name)
+
+    print(f"\nLe fichier STL '{output_file_name}' a été créer.")
 
 
-# Main program
-#file_name = input("Enter the name of the STL file: ")
-file_name = input("Entrer nom du fichier STL: ")
 
-# Load the original STL file
-original_mesh = mesh.Mesh.from_file(file_name)
+def main():
+    while True:
+        file_name = input("Entrer le nom du fichier STL: ")
 
-# Print information about the STL file
-num_triangles = len(original_mesh.vectors)
-min_z = np.min(original_mesh.vectors[:, :, 2])
-max_z = np.max(original_mesh.vectors[:, :, 2])
-#print(f"STL file '{file_name}' contains {num_triangles} triangles.")
-#print(f"Minimum Z value: {min_z}")
-#print(f"Maximum Z value: {max_z}")
-print(f"le Fichier STL contiens {num_triangles} triangles.")
-print(f"Valeur de Z minimal: {min_z}")
-print(f"Valeur de Z maximal: {max_z}")
+        loading_spinner.stop_event = threading.Event()
+        loading_thread = threading.Thread(target=loading_spinner)
+        loading_thread.start()
 
-#fixed_z = float(input("Enter the fixed Z value for new edges and floor: "))
-fixed_z = float(input("Entrer la valeur en Z du sole: "))
+        # Load the STL file
+        original_mesh = mesh.Mesh.from_file(file_name)
 
-# Find free edges
-free_edges = find_free_edges(original_mesh.vectors)
+        loading_spinner.stop_event.set()
+        loading_thread.join()
 
-# Create new edges
-new_edges = create_new_edges(free_edges, fixed_z)
+        num_triangles = len(original_mesh.vectors)
+        min_z = np.min(original_mesh.vectors[:, :, 2])
+        max_z = np.max(original_mesh.vectors[:, :, 2])
+        print(f"\nNombre de triangles: {num_triangles}")
+        print(f"Valeur Z maximal: {min_z}")
+        print(f"Valeur Z minimal: {max_z}")
 
-# Create triangles for walls
-wall_triangles = create_triangles(free_edges, new_edges)
+        fixed_z = float(input("Enter la hauteur en Z du sole: "))
 
-# Create triangles for the floor
-floor_triangles = create_floor(new_edges, fixed_z)
+        # Find free edges
+        free_edges = find_free_edges(original_mesh.vectors)
 
-# Combine original triangles, wall triangles, and floor triangles
-all_triangles = original_mesh.vectors.tolist() + wall_triangles + floor_triangles
+        # Create new edges
+        new_edges = create_new_edges(free_edges, fixed_z)
 
-# Export the final STL file
-output_file_name = "output.stl"
-export_stl(all_triangles, original_mesh, output_file_name)
+        # Create triangles for walls
+        wall_triangles = create_triangles(free_edges, new_edges)
 
-print(f"STL file '{output_file_name}' has been created.")
+        # Create triangles for the floor
+        floor_triangles = create_floor(new_edges, fixed_z)
+
+        # Combine original triangles, wall triangles, and floor triangles
+        all_triangles = original_mesh.vectors.tolist() + wall_triangles + floor_triangles
+
+        # Export the final STL file
+        export_stl(all_triangles, original_mesh, file_name)
+
+        # Ask the user if they want to process another file
+        choice = input("\nVoulez-vous extruder un autre fichier? (o/n): ")
+        if choice.lower() != "o":
+            break
+        print("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
+
+
+if __name__ == '__main__':
+    main()
